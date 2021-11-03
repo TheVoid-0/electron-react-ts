a0c65c23c6015a2ea5de5639dd2f68a88af5d6c5
TheVoid-0 <marcodnmoura@gmail.com>
2021-11-02 23:03:47 -0300

    Adjust Routes variables

--- electron/common/services/ipc-main.service.ts
+++ electron/common/services/ipc-main.service.ts
@@ -16,45 +16,45 @@ export class IpcMainService {
     }
 
     /**
-     * Cria um listener que aguarda o sinal de que uma página precisa dos recursos desse módulo;
+     * Cria um listener que aguarda o sinal de que uma módulo precisa dos recursos desse módulo;
      * 
-     * @param page nome da página na qual o electron espera ser chamado.
-     * @param listener callback que será chamado quando uma "requisição" para essa página chegar, pode conter qualquer lógica de negócio e inicializar outros
+     * @param module nome da módulo no qual o electron espera ser chamado.
+     * @param listener callback que será chamado quando uma "requisição" para esse módulo chegar, pode conter qualquer lógica de negócio e inicializar outros
      * listeners que aguardam novas requisições
      */
-    public initializePageListener(page: string, listener: (event: IpcMainEvent, ...args: any[]) => void) {
-        // Garante que haverá somente um listener da página no main process do electron
-        this.removeAllFromPage(page);
-        this._ipcMain.on(page, listener);
+    public initializePageListener(module: string, listener: (event: IpcMainEvent, ...args: any[]) => void) {
+        // Garante que haverá somente um listener da módulo no main process do electron
+        this.removeAllFromPage(module);
+        this._ipcMain.on(module, listener);
     }
 
     /**
      * 
-     * @param page Nome do canal/página/módulo "pai" que guardará todos os "subcanais" para eventos que acontecem nessa página/módulo.
-     * EX: dado o módulo cliente, você pode incializar o listener para esse módulo passando para esse parâmetro a string 'cliente-page',
+     * @param module Nome do canal/página/módulo "pai" que guardará todos os "subcanais" para eventos que acontecem nessa página/módulo.
+     * EX: dado o módulo cliente, você pode incializar o listener para esse módulo passando para esse parâmetro a string 'cliente-module',
      * e então, pode adicionar listeners desse módulo passando para esta função essa mesma string e um outro canal que responderá pela funcionalidade
-     * específica: ...on('cliente-page', 'cliente-page-find-all', listener)
+     * específica: ...on('cliente-module', 'cliente-module-find-all', listener)
      * @param channel Nome do canal que aguardará ser chamado para invocar a função listener
      * @param listener Função que será invocada quando o channel for chamado
      */
-    public on(page: string, channel: string, listener: (event: IpcMainEvent, ...args: any[]) => void): void {
+    public on(module: string, channel: string, listener: (event: IpcMainEvent, ...args: any[]) => void): void {
         if (!this._ipcMain) {
             return;
         }
         this._ipcMain.on(channel, listener);
 
-        // Verifica se já existe um listener dessa página, se sima adiciona esse canal aos listeners dessa página, se não, cria a página com esse canal como listener
-        this.listeners[page] ? this.listeners[page].push(channel) : this.listeners = { [page]: [channel] };
+        // Verifica se já existe um listener desse página, se sima adiciona esse canal aos listeners dessa módulo, se não, cria a módulo com esse canal como listener
+        this.listeners[module] ? this.listeners[module].push(channel) : this.listeners = { [module]: [channel] };
     }
 
-    public removeAllFromPage(page: string): void {
-        if (!this.listeners[page]) return;
+    public removeAllFromPage(module: string): void {
+        if (!this.listeners[module]) return;
 
-        for (const channel of this.listeners[page]) {
+        for (const channel of this.listeners[module]) {
             this._ipcMain?.removeAllListeners(channel);
         }
-        // NÃO remover o listener da própria página, pois ele poderá ser chamado para recriar as rotas!!
-        // this._ipcMain.removeAllListeners(page);
-        // delete this.listeners[page];
+        // NÃO remover o listener da própria módulo, pois ele poderá ser chamado para recriar as rotas!!
+        // this._ipcMain.removeAllListeners(module);
+        // delete this.listeners[module];
     }
 }
